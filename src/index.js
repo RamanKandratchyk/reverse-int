11f0737bb282467e76ca6118a57d50469d2ead36
Raman Kandratchyk <kondrator@gmail.com>
2022-06-28 13:38:16 +0300

    feat: add function reverse(n), 100% passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let strN = String(Math.abs(n));
+  let L = strN.length;
+  let N = [];
+  let result = '';
+
+  for (let i = 0; i < L; i++) {
+    N[i] = strN[L - 1 - i];
+  };
+
+  result = N.join('');
+
+  for (let i = 0; i < L; i++) {
+    if ((result[0] === '0') && (result.length > 1)) {
+      result = result.slice(1);
+    };
+  };
+
+  return result;
 }
