d3640b83f212bb2a75eb822ab52189b6c04e4ce8
Raman Kandratchyk <kondrator@gmail.com>
2023-11-05 22:52:14 +0300

    feat: improve solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
+// module.exports = function reverse(n) {
+//     let strN = String(Math.abs(n));
+//     let L = strN.length;
+//     let N = [];
+//     let result = "";
+
+//     for (let i = 0; i < L; i++) {
+//         N[i] = strN[L - 1 - i];
+//     }
+
+//     result = N.join("");
+
+//     for (let i = 0; i < L; i++) {
+//         if (result[0] === "0" && result.length > 1) {
+//             result = result.slice(1);
+//         }
+//     }
+
+//     return result;
+// };
+
 module.exports = function reverse(n) {
-  let strN = String(Math.abs(n));
-  let L = strN.length;
-  let N = [];
-  let result = '';
-
-  for (let i = 0; i < L; i++) {
-    N[i] = strN[L - 1 - i];
-  };
-
-  result = N.join('');
-
-  for (let i = 0; i < L; i++) {
-    if ((result[0] === '0') && (result.length > 1)) {
-      result = result.slice(1);
-    };
-  };
-
-  return result;
-}
+    return +String(Math.abs(n)).split("").reverse().join("");
+};
